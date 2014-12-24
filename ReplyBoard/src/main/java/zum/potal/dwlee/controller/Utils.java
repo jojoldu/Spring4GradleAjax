6b2b65be6691b2f1f4822c6f80659faaa9dd6c86
이동욱 <jojoldu@zuminternet.com>
2014-12-24 09:39:53 +0900

    no message

---
+++ ReplyBoard/src/main/java/zum/potal/dwlee/controller/Utils.java
@@ -0,0 +1,13 @@
+package zum.potal.dwlee.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+	public static String getNowTime(){
+		long time = System.currentTimeMillis(); 
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		return dayTime.format(new Date(time));
+	}
+}
