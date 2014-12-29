3f1d8a9d7faa47faeff52ae5c66ca0ab6384197a
이동욱 <jojoldu@zuminternet.com>
2014-12-29 15:32:35 +0900

    문자열 비교 수정

--- ReplyBoard/src/main/java/zum/potal/dwlee/config/Interceptor.java
+++ ReplyBoard/src/main/java/zum/potal/dwlee/config/Interceptor.java
@@ -11,10 +11,10 @@ public class Interceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler){
 		String reqUrl = request.getRequestURI().toString();
 		try{
-			if(reqUrl.equals("/ReplyBoard/")){
+			if("/ReplyBoard/".equals(reqUrl)){
 				return true;
 			}else{
-				if(reqUrl.matches(".*.json")){ // json 데이터 요청의 경우 통과 
+				if(".*.json".matches(reqUrl)){ // json 데이터 요청의 경우 통과 
 					return true; 
 				}else{//페이지 이동의 경우
 					if(request.getSession().getAttribute("loginVO") == null){//세션이 없다면
--- ReplyBoard/src/main/java/zum/potal/dwlee/config/WebInitializer.java
+++ ReplyBoard/src/main/java/zum/potal/dwlee/config/WebInitializer.java
@@ -15,7 +15,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
  
   @Override
   protected Class<?>[] getServletConfigClasses() {
-      return new Class<?>[] { WebConfig.class };
+      return new Class<?>[] { WebConfig.class, SecurityConfig.class };
   }
  
   //servlet-mapping
